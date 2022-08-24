aef1f7c2443d34df1ba1b6ae6a3af8b6bbf7c099
theoddorrrrr <aidar140702@mail.ru>
2022-08-24 23:07:09 +0600

    addeed routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.scss";
+import App from "./components/App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        {/* <Route index element={<Home />} /> */}
+        {/* <Route path="teams" element={<Teams />}>
+        <Route path=":teamId" element={<Team />} />
+        <Route path="new" element={<NewTeamForm />} />
+        <Route index element={<LeagueStandings />} />
+      </Route> */}
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
-

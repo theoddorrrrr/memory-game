bebee31d15af12055d6afa7de51a051a992576be
theoddorrrrr <aidar140702@mail.ru>
2022-08-27 23:09:32 +0600

    added cards

--- src/data/index.js
+++ src/data/index.js
@@ -1,26 +1,35 @@
 export const data = [
     {
-      title: "pikachu",
-      img: "https://w7.pngwing.com/pngs/104/341/png-transparent-pokemon-let-s-go-pikachu-ash-ketchum-pokemon-pikachu-pikachu-let-s-go-ash-ketchum-pokemon-pikachu.png",
+      title: "tanjiro",
+      img: "https://i.pinimg.com/564x/a7/9a/f3/a79af3e39d27a88ad0e1290364b14c78.jpg",
     },
     {
-      title: "Jiggypluff",
-      img: "https://w7.pngwing.com/pngs/488/194/png-transparent-jigglypuff-illustration-pokemon-go-pikachu-jigglypuff-wigglytuff-pokemon-go-mammal-cat-like-mammal-carnivoran.png",
+      title: "izuku",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq8VJOBxBmMkxyPFhnXLwoE9XvpV-wf8qrg3yJiV5b7nv22RZINlq9sxrlw22WWiqM7Uk&usqp=CAU",
     },
     {
-      title: "Eevee",
-      img: "https://w7.pngwing.com/pngs/288/96/png-transparent-pokemon-go-pokemon-platinum-pikachu-pokemon-channel-eevee-eevee-mammal-carnivoran-paw.png",
+      title: "saitama",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3MLx_zxI3kuNdOfQwj5uz1-wVgr9quwS4EoelV2c7mLRVH4HcA-HGk0NFfljv3yXMXQc&usqp=CAU",
     },
     {
-      title: "Squirtle",
-      img: "https://e7.pngegg.com/pngimages/965/769/png-clipart-pokemon-pokemon-thumbnail.png",
+      title: "jujutsu kaisen",
+      img: "https://lyricstranslate.com/files/styles/artist/public/jujutsu-kaisen-soundtrack-cover.jpg",
     },
     {
-      title: "Red And Blue",
-      img: "https://e7.pngegg.com/pngimages/461/963/png-clipart-pokemon-red-and-blue-pokemon-x-and-y-pokemon-go-haunter-pokemon-go-purple-violet.png",
+      title: "gojo",
+      img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3d305e75-d2b9-4a65-bdef-6414c694d540/dea7puj-ac24083c-e14a-4902-b8a7-67dddf56b8bc.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzNkMzA1ZTc1LWQyYjktNGE2NS1iZGVmLTY0MTRjNjk0ZDU0MFwvZGVhN3B1ai1hYzI0MDgzYy1lMTRhLTQ5MDItYjhhNy02N2RkZGY1NmI4YmMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BoXxNKpK2xOL1lzHRjx8GfBa0-aEqsqdzBaJ8xz5NLc",
     },
     {
-      title: "Psyduck",
-      img: "https://www.clipartmax.com/png/middle/306-3062614_never-miss-a-moment-psyduck-pokemon.png",
+      title: "hyakkimaru",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2QZx2DNpUkES0RELYSCK9V8BW1YgLmfMZlP08Wz2mC10czOMh8EP_RTaLXFZ89kl87QI&usqp=CAU",
     },
+    {
+      title: "kaori",
+      img: "https://i.pinimg.com/736x/7f/db/76/7fdb769203b8112da1172f5a1f8ee89a.jpg",
+    },
+    {
+      title: "makima",
+      img: "https://i1.sndcdn.com/artworks-Tfrxay9eGxlsHfBT-BiJD1w-t500x500.jpg",
+    },
+    
   ];
